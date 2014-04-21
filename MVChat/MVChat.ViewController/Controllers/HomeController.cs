c1818c0a43aa669397e0a1b93beaec8ac7656249
Odonno <bottiau.david@laposte.net>
2014-04-21 12:24:23 +0200

    Add HomeController (with Index and Login methods)

---
+++ MVChat/MVChat.ViewController/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVChat.ViewController.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Login
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
