9a5b129dc117eac914effb5b9acbdb9c33513934
Odonno <bottiau.david@laposte.net>
2014-04-21 20:28:18 +0200

    Implement PostMessage action in HomeController

--- MVChat/MVChat.ViewController/Controllers/HomeController.cs
+++ MVChat/MVChat.ViewController/Controllers/HomeController.cs
@@ -11,8 +11,19 @@ namespace MVChat.ViewController.Controllers
     {
         #region Fields
 
-        private List<User> _users;
-        private List<Message> _messages;
+        private static List<User> _users = new List<User>
+        {
+            new User {Name = "dbottiau"},
+            new User {Name = "jbvigneron"}
+        };
+
+        private static List<Message> _messages = new List<Message>
+        {
+            new Message {Writer = _users[0], Text = "Ey !", Date = new DateTime(2014, 4, 21, 13, 27, 42)},
+            new Message {Writer = _users[1], Text = "Ca va ?", Date = new DateTime(2014, 4, 21, 13, 27, 57)},
+            new Message {Writer = _users[0], Text = "T'as vu Cortana ?", Date = new DateTime(2014, 4, 21, 13, 28, 1)},
+            new Message {Writer = _users[1], Text = "Yep", Date = new DateTime(2014, 4, 21, 13, 28, 13)}
+        };
 
         #endregion
 
@@ -21,19 +32,6 @@ namespace MVChat.ViewController.Controllers
 
         public HomeController()
         {
-            _users = new List<User>
-            {
-                new User {Name = "dbottiau"},
-                new User {Name = "jbvigneron"}
-            };
-
-            _messages = new List<Message>
-            {
-                new Message {Writer = _users[0], Text = "Ey !", Date = new DateTime(2014, 4, 21, 13, 27, 42)},
-                new Message {Writer = _users[1], Text = "Ca va ?", Date = new DateTime(2014, 4, 21, 13, 27, 57)},
-                new Message {Writer = _users[0], Text = "T'as vu Cortana ?", Date = new DateTime(2014, 4, 21, 13, 28, 1)},
-                new Message {Writer = _users[1], Text = "Yep", Date = new DateTime(2014, 4, 21, 13, 28, 13)}
-            };
         }
 
         #endregion
@@ -61,10 +59,24 @@ namespace MVChat.ViewController.Controllers
                 Session["user"] = existingUser;
             else
             {
-                var newUser = new User {Name = name};
+                var newUser = new User { Name = name };
                 _users.Add(newUser);
                 Session["user"] = newUser;
             }
+
+            return RedirectToAction("Index");
+        }
+
+        //
+        // POST: /Home/PostMessage
+
+        [HttpPost]
+        public ActionResult PostMessage(Message message)
+        {
+            message.Date = DateTime.Now;
+            message.Writer = Session["user"] as User;
+
+            _messages.Add(message);
 
             return RedirectToAction("Index");
         }
