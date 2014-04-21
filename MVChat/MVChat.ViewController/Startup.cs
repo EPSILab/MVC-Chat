4acd06a68dfbf4b2a3d7928e9e59a9539dcb097d
Odonno <bottiau.david@laposte.net>
2014-04-22 01:12:14 +0200

    Add SignalR package

---
+++ MVChat/MVChat.ViewController/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace MVChat.ViewController
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
