8246babe48696ef14f9ca70635c083a6b57d4068
Odonno <bottiau.david@laposte.net>
2014-04-21 13:30:22 +0200

    Add fields in HomeController, initialized in constructor

--- MVChat/MVChat.ViewController/Controllers/HomeController.cs
+++ MVChat/MVChat.ViewController/Controllers/HomeController.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVChat.Model;
 
 namespace MVChat.ViewController.Controllers
 {
     public class HomeController : Controller
     {
+#region Fields
+
+        private IList<User> _users;
+        private IList<Message> _messages; 
+
+#endregion
+
+
+#region Constructor
+
+        public HomeController()
+        {
+            _users = new List<User>
+            {
+                new User {Name = "dbottiau"},
+                new User {Name = "jbvigneron"}
+            };
+
+            _messages = new List<Message>
+            {
+                new Message {Writer = _users[0], Text = "Ey !", Date = new DateTime(2014, 4, 21, 13, 27, 42)},
+                new Message {Writer = _users[1], Text = "Ca va ?", Date = new DateTime(2014, 4, 21, 13, 27, 57)},
+                new Message {Writer = _users[0], Text = "T'as vu Cortana ?", Date = new DateTime(2014, 4, 21, 13, 28, 1)},
+                new Message {Writer = _users[1], Text = "Yep", Date = new DateTime(2014, 4, 21, 13, 28, 13)}
+            };
+        }
+
+#endregion
+
+
+#region Action methods
+
         //
         // GET: /Home/
 
@@ -25,5 +58,6 @@ namespace MVChat.ViewController.Controllers
             return View();
         }
 
+#endregion
     }
 }
