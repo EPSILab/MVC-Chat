6d5ee0a6a0da12534ec0b8dbf43722e6e51dcb74
Odonno <bottiau.david@laposte.net>
2014-04-22 02:00:18 +0200

    Add a MessageHub implementation / Implement functions of Hubs in Index view too

--- MVChat/MVChat.Model/Message.cs
+++ MVChat/MVChat.Model/Message.cs
@@ -5,7 +5,7 @@ namespace MVChat.Model
     public class Message
     {
         public string Title { get; set; }
-        public string Text { get; set; }
+        public string Content { get; set; }
         public DateTime Date { get; set; }
         public User Writer { get; set; }
     }
---
+++ MVChat/MVChat.ViewController/Hubs/MessageHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using MVChat.Model;
+
+namespace MVChat.ViewController.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public void SendMessage(Message message)
+        {
+            message.Date = DateTime.Now;
+            Clients.All.showMessage(message);
+        }
+    }
+}
